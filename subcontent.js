41cf2fa59a47c4afca4703dddd8e1406a4afd760
igodzp <saranyu_phusit@hotmail.com>
2015-02-25 02:29:23 -0500

    Add UI for forum helper

--- subcontent.js
+++ subcontent.js
@@ -1,16 +1,69 @@
 $(document).ready(function(){
 
   var f = function(){
-        var buttonOnSite = document.getElementById("cke_contents_vB_Editor_QR_editor");
-        if(buttonOnSite != undefined){
-          parent = buttonOnSite.parentElement;
-          next = buttonOnSite.nextSibling;
-          button = document.createElement("button");
-          text = document.createTextNode("test");
+      
+        var target = document.getElementById("vB_Editor_QR");
+        if(target != undefined){
+          parent = target.parentElement;
+          next = target.nextSibling;
+          button = document.createElement("div");
+          
+        
+          
+          button.innerHTML = '<img id="image" class="toggler" src="' + chrome.extension.getURL('maplefm-icon.png') + '" style="width: 25px; top: -50px; right: 10px;" />';
+          button.className = "inject-btn";
+          
+          
+          parent.insertBefore(button, target);
+          /*if (next) parent.insertBefore(button, next);
+          else parent.appendChild(button);*/
+          
+          // insert form 
+          searchform = document.createElement("form");
+          searchform.className = "formwrap";
+          searchform.innerHTML = '<input type="text" id="inject-form">';
+          button.appendChild(searchform);
+          //parent.insertBefore(searchform,button);
+          
+          $('.toggler').click(function(){
+             if( ! $('.inject-btn').hasClass("showForm") ){
+                $('.inject-btn').addClass("showForm");
+                $('#inject-form').focus();
+             }
+             else{
+                $('.inject-btn').removeClass("showForm");
+             }
+          });
+          
+          
+          $.getJSON("http://maple.fm/api/list/items", function(data) {
+            
+            var items = data.map(function(x) {
+              return x.b
+            });
 
-          button.appendChild(text);
-          if (next) parent.insertBefore(button, next);
-          else parent.appendChild(button);
+            $("#inject-form").autocomplete({
+              source: function(request, response) {
+                var results = $.ui.autocomplete.filter(items, request.term);
+                response(results.slice(0, 7));
+              },
+              messages: {
+                noResults: '',
+                results: function() {}
+              },
+              select: function() {
+                setTimeout(function() {
+
+                }, 200);
+              }
+
+            });
+          });
+          
+          
+          
+          
+          
           return ;
         }
 
